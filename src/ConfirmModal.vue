d71e1bb6a8506a67c936f84c1e434854adaa76cd
Mr. W <mrw@vueform.com>
2023-10-25 11:02:37 +0200

    fix: default classes for buttons in modal

--- src/ConfirmModal.vue
+++ src/ConfirmModal.vue
@@ -1,8 +1,8 @@
 <template>
   <div data-vf-toggle-confirm-wrapper>
     <div data-vf-toggle-confirm-text v-html="props.confirmText"></div>
-    <button ref="toggleConfirmButton" data-vf-toggle-confirm-confirm class="vf-btn vf-btn-primary vf-">{{ props.confirmButton }}</button>
-    <button ref="toggleCancelButton" data-vf-toggle-confirm-cancel class="vf-btn vf-btn-secondary">{{ props.cancelButton }}</button>
+    <button ref="toggleConfirmButton" data-vf-toggle-confirm-confirm class="inline-block transition form-border-width-btn form-shadow-btn focus:outline-zero form-bg-btn form-color-btn form-border-color-btn form-p-btn form-radius-btn form-text cursor-pointer transition-transform ease-linear focus:form-ring transform hover:scale-105">{{ props.confirmButton }}</button>
+    <button ref="toggleCancelButton" data-vf-toggle-confirm-cancel class="inline-block transition form-border-width-btn form-shadow-btn focus:outline-zero form-bg-btn-secondary form-color-btn-secondary form-border-color-btn-secondary form-p-btn form-radius-btn form-text cursor-pointer transition-transform ease-linear focus:form-ring transform hover:scale-105">{{ props.cancelButton }}</button>
     <button ref="toggleEscapeButton" data-vf-toggle-confirm-escape />
   </div>
 </template>
