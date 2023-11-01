70fb6c79c7714a90d2ffc92448ca482a6e8064f4
Adam Berecz <adam@laraform.io>
2023-11-01 11:45:56 +0100

    feat: ui upgrades

--- ConfirmModal.vue
+++ ConfirmModal.vue
@@ -65,49 +65,49 @@ const handleCloseClick = () => {
 </script>
 
 <style lang="scss">
-*, :root, :before, :after {
-  --vf-toggle-confirm-modal-px: 1.5rem;
-  --vf-toggle-confirm-modal-py: 1.5rem;
-}
-
 .vf-toggle-confirm-modal-overlay {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgb(107 114 128 / .75);
-  z-index: 999;
+  background: var(--vf-toggle-confirm-modal-overlay-bg, rgb(107 114 128 / .75));
+  z-index: var(--vf-toggle-confirm-modal-overlay-z, 999);
+  overflow: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .vf-toggle-confirm-modal-wrapper {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  min-width: 380px;
-  min-height: 120px;
-  max-width: 512px;
+  position: relative;
+  margin: 1rem;
+  min-width: var(--vf-toggle-confirm-modal-min-width, 380px);
+  max-width: var(--vf-toggle-confirm-modal-max-width, 512px);
+  min-height: var(--vf-toggle-confirm-modal-min-height, 120px);
   width: 100%;
-  background: #FFFFFF;
-  padding: var(--vf-toggle-confirm-modal-py) var(--vf-toggle-confirm-modal-px);
-  box-shadow: 0 20px 25px -5px #0000001a, 0 8px 10px -6px #0000001a;
+  background: var(--vf-toggle-confirm-modal-bg, #FFFFFF);
+  padding: var(--vf-toggle-confirm-modal-py, 1.5rem) var(--vf-toggle-confirm-modal-px, 1.5rem);
+  box-shadow: var(--vf-toggle-confirm-modal-shadow, 0 20px 25px -5px #0000001a, 0 8px 10px -6px #0000001a);
 }
 
 .vf-toggle-confirm-modal-title {
-  font-size: 24px;
-  line-height: 1.2;
-  padding-bottom: var(--vf-toggle-confirm-modal-py);
-  font-weight: 500;
+  font-size: var(--vf-toggle-confirm-modal-title-font-size, 24px);
+  line-height: var(--vf-toggle-confirm-modal-title-line-height, 1.2);
+  font-weight: var(--vf-toggle-confirm-modal-title-font-weight, 500);
+  padding-bottom: var(--vf-toggle-confirm-modal-title-pb, 1.5rem);
+  margin-right: var(--vf-toggle-confirm-modal-close-width, 1.5rem);
 }
 
 .vf-toggle-confirm-modal-content {
+  font-size: var(--vf-toggle-confirm-modal-font-size, 1rem);
+  color: var(--vf-toggle-confirm-modal-color, inherit);
 }
 
 .vf-toggle-confirm-modal-buttons-wrapper {
-  padding-top: var(--vf-toggle-confirm-modal-py);
+  padding-top: var(--vf-toggle-confirm-modal-buttons-pt, 1.5rem);
   display: flex;
-  gap: 0.75rem;
+  gap: var(--vf-toggle-confirm-modal-buttons-gap, 0.75rem);
 }
 
 .vf-toggle-confirm-modal-confirm {
@@ -163,20 +163,20 @@ const handleCloseClick = () => {
 
 .vf-toggle-confirm-modal-close {
   position: absolute;
-  top: var(--vf-toggle-confirm-modal-py);
-  right: var(--vf-toggle-confirm-modal-px);
+  top: var(--vf-toggle-confirm-modal-py, 1.5rem);
+  right: var(--vf-toggle-confirm-modal-px, 1.5rem);
   cursor: pointer;
   mask-repeat: no-repeat;
   mask-position: center center;
   mask-size: contain;
-  mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"/></svg>');
-  background-color: var(--vf-gray-400);
-  width: 1.5rem;
-  height: 1.5rem;
+  mask-image: var(--vf-toggle-confirm-modal-close-icon, url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"/></svg>'));
+  background-color: var(--vf-toggle-confirm-modal-close-color, var(--vf-gray-400));
+  width: var(--vf-toggle-confirm-modal-close-width, 1.5rem);
+  height: var(--vf-toggle-confirm-modal-close-height, 1.5rem);
 }
 
 .vf-toggle-confirm-modal-close:hover {
-  background-color: var(--vf-gray-500);
+  background-color: var(--vf-toggle-confirm-modal-, var(--vf-gray-500));
   transition: background-color .3s;
 }
 </style>
